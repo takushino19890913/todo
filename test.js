2a670645df26e5627ec1b35d1d355554c11a7170
Takuya <931shinotaku@gmail.com>
2020-03-15 14:04:58 +0000

    .gitignoreにtasks.json指定。test.jsでtasks.jsonを無視する設定。

--- test.js
+++ test.js
@@ -1,22 +1,25 @@
 "use strict";
 const todo = require('./index.js');
 const assert = require('assert');
+const fs = require('fs');
 
-//todo,listのテスト
-todo.todo('Buy apples');
-todo.todo('Get a coffee');
-assert.deepEqual(todo.list(),['Buy apples', 'Get a coffee']);
+fs.unlink('./tasks.json',(err) =>{
+  //todo,listのテスト
+  todo.todo('Buy apples');
+  todo.todo('Get a coffee');
+  assert.deepEqual(todo.list(),['Buy apples', 'Get a coffee']);
 
-//done,donelistのテスト
-assert.deepEqual(todo.done('Buy apples'),true);
-assert.deepEqual(todo.done('No tasks'),false);
-assert.deepEqual(todo.donelist(),['Buy apples']);
-assert.deepEqual(todo.list(), ['Get a coffee']);
+  //done,donelistのテスト
+  assert.deepEqual(todo.done('Buy apples'),true);
+  assert.deepEqual(todo.done('No tasks'),false);
+  assert.deepEqual(todo.donelist(),['Buy apples']);
+  assert.deepEqual(todo.list(), ['Get a coffee']);
 
-//delのテスト
-todo.del('Buy apples');
-assert.deepEqual(todo.del('Get a coffee'),true);
-assert.deepEqual(todo.del('No tasks'), false);
-assert.deepEqual(todo.donelist(),[]);
+  //delのテスト
+  todo.del('Buy apples');
+  assert.deepEqual(todo.del('Get a coffee'),true);
+  assert.deepEqual(todo.del('No tasks'), false);
+  assert.deepEqual(todo.donelist(),[]);
 
-console.log('test is finished');
+  console.log('test is finished');
+});
